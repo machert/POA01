dde380c020650051fe1c37db0f4e029cd8b4b328
Lucas <lucascarlos@protonmail.com>
2016-10-22 17:04:48 -0200

    0.0.4

--- controller.js
+++ controller.js
@@ -2,85 +2,51 @@ angular.module('poupanca', [])
 
 .controller('PoupancaController', function ($scope) {
 
-    const controller = this;
+    const controller = this
 
-    $scope.contas = [
-        {
-            nome: 'John Rambo',
-            banco: 'Banestes',
-            agencia: 12,
-            numero: '21000-100',
-            saldo: 1000,
-            juros: 0.1,
-            taxa: 10
-        },
-                {
-            nome: 'Maria',
-            banco: 'Banco do Brasil',
-            agencia: 22,
-            numero: '22000-200',
-            saldo: 2000,
-            juros: 0.2,
-            taxa: 20
-        },
-        {
-            nome: 'Ana',
-            banco: 'Bradesco',
-            agencia: 32,
-            numero: '23000-300',
-            saldo: 3000,
-            juros: 0.3,
-            taxa: 30
-        }        
-    ]
+    $scope.conta = {}
+
+    controller.listar = function () {
+
+        listarContas()
+            .then(function (result) {
+
+                const contas = Object.keys(result).map(function (key) { return result[key] })
+
+                $scope.contas = contas
+
+                $scope.$apply()
+
+            })
+            .catch(function (error) {
+
+                console.log(error)
+
+            })
+    }
 
     controller.salvar = function (conta) {
-        
-        console.log(
-            'conta.nome: ' + conta.nome + '\n' +
-            'conta.banco: ' + conta.banco + '\n' +
-            'conta.agencia: ' + conta.agencia + '\n' +
-            'conta.numero: ' + conta.numero + '\n' +
-            'conta.saldo: ' + conta.saldo + '\n' +
-            'conta.juros: ' + conta.juros + '\n' +
-            'conta.taxa: ' + conta.taxa + '\n\n'
-        )
 
-        let poupanca = new Poupanca (   conta.nome,
-                                        conta.banco,
-                                        conta.agencia,
-                                        conta.numero,
-                                        parseFloat(conta.saldo),
-                                        parseFloat(conta.juros),
-                                        parseFloat(conta.taxa)
-        )
+        // falta salvar conta nova ou editada
 
-        console.log(
-                    'poupanca.nome: ' + poupanca.nome + '\n' +
-                    'poupanca.banco: ' + poupanca.banco + '\n' +
-                    'poupanca.agencia: ' + poupanca.agencia + '\n' +
-                    'poupanca.numero: ' + poupanca.numero + '\n' +
-                    'poupanca.saldo: ' + poupanca.saldo + '\n' +
-                    'poupanca.juros: ' + poupanca.juros + '\n' +
-                    'poupanca.taxa: ' + poupanca.taxa + '\n\n'
-        )                                    
+        controller.listar()
 
-        poupanca.atualizaSaldo1()
-
-        poupanca.atualizaSaldo2(0.01)
-
-        poupanca.atualizaSaldo3(1, 0.1)
-
-        poupanca.operacao(1, 100)
-
-        poupanca.operacao(2, 200)
-
-        poupanca.operacao(3, 0.05)
-
-        console.log(SalvarDAO(poupanca))
-
-        // $scope.conta = {}
-
+        $scope.conta = {}
     }
+
+    controller.editar = function (linha) {
+
+        const conta = linha.conta
+
+        $scope.conta.nome = conta.nome
+        $scope.conta.banco = conta.banco
+        $scope.conta.agencia = conta.agencia
+        $scope.conta.numero = conta.numero
+        $scope.conta.saldo = conta.saldo
+        $scope.conta.juros = conta.juros
+        $scope.conta.taxa = conta.taxa
+    }
+
+    controller.listar()
 
 });
--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
-var config = {
+const config = {
     apiKey: "AIzaSyAGpgGcXZRaO934fQy9AnlZ6GvYYjC9Kd8",
     authDomain: "poajs-ffde8.firebaseapp.com",
     databaseURL: "https://poajs-ffde8.firebaseio.com",
     storageBucket: "poajs-ffde8.appspot.com",
     messagingSenderId: "910970669779"
-};
+}
 
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
-function SalvarDAO(poupanca) {
+function salvarConta(poupanca) {
     try {
         firebase.database().ref('contas/').push({
             nome: poupanca.nome,
@@ -18,9 +18,31 @@ function SalvarDAO(poupanca) {
             saldo: poupanca.saldo,
             juros: poupanca.juros,
             taxa: poupanca.taxa
-        });
+        })
     } catch (err) {
         return err.message;
     }
     return 'Sucesso';
+}
+
+function listarContas() {
+
+    return new Promise(function (resolve, reject) {
+
+        firebase.database().ref('/contas').once('value')
+            .then(function (snapshot) {
+
+                let contas = snapshot.val()
+
+                resolve(contas)
+
+            })
+            .catch(function (error) {
+
+                reject(error)
+
+            })
+
+    })
+
 }
