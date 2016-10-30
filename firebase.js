5535a8d3d4ddca439880502ffe1ca432dc737f74
machert <felipemachert@gmail.com>
2016-10-30 15:52:06 -0200

    Delete firebase.js

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    apiKey: "AIzaSyAGpgGcXZRaO934fQy9AnlZ6GvYYjC9Kd8",
-    authDomain: "poajs-ffde8.firebaseapp.com",
-    databaseURL: "https://poajs-ffde8.firebaseio.com",
-    storageBucket: "poajs-ffde8.appspot.com",
-    messagingSenderId: "910970669779"
-}
-
-firebase.initializeApp(config)
